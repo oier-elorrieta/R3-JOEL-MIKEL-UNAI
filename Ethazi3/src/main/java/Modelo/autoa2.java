d0f2d0c7fb28074ea4898386993ccaab71ca700b
joelfano <joel.fanode@elorrieta-errekamari.com>
2021-01-15 14:30:53 +0100

    GitIgnore proba

---
+++ Ethazi3/src/main/java/Modelo/autoa2.java
@@ -0,0 +1,47 @@
+package Modelo;
+
+public class autoa2 {
+
+	private String matrikula;
+	private String marka;
+	private String modelo;
+	private String kolorea;
+
+	public autoa2(String matrikula, String marka, String modelo, String kolorea) { 
+		this.matrikula = matrikula;
+		this.marka = marka;
+		this.modelo = modelo;
+		this.kolorea = kolorea;
+	}
+
+	public String getMatrikula() {
+		return matrikula;
+	}
+	public void setMatrikula(String matrikula) {
+		this.matrikula = matrikula;
+	}
+	public String getMarka() {
+		return marka;
+	}
+	public void setMarka(String marka) {
+		this.marka = marka;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getKolorea() {
+		return kolorea;
+	}
+	public void setKolorea(String kolorea) {
+		this.kolorea = kolorea;
+	}
+
+	@Override
+	public String toString() {
+		return "autoa [matrikula=" + matrikula + ", marka=" + marka + ", modelo=" + modelo + ", kolorea=" + kolorea
+				+ "]";
+	}
+}
