67233ce9ce18d0106593f5b5541a7ae1b14ea3da
in1dam <in1dam@elorrieta.int>
2021-01-22 09:33:15 +0100

    Menua eginda

--- Ethazi3/src/main/java/Vista/PanelMenu.java
+++ Ethazi3/src/main/java/Vista/PanelMenu.java
@@ -1,69 +1,73 @@
 package Vista;
 
+
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox; 
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 import Controlador.ControladorPanelMenu; 
-
+import javax.swing.JPanel;
 @SuppressWarnings("serial")
-public class PanelMenu extends JPanel{
+public class PanelMenu extends JPanel {
 
-	private JButton btnfaktura;
-	private JButton btnticket;
-	private JButton btnpedidos;
-	//private JButton ;
-	private ControladorPanelMenu controladorPanelMenu;
-
-	public PanelMenu(ControladorPanelMenu controladorPanelMenu)
-	{
-		this.controladorPanelMenu = controladorPanelMenu;
-
+	/**
+	 * Create the panel.
+	 */
+	public PanelMenu() {
 		setLayout(null);
-
-		btnfaktura = new JButton("Faktura");
-		btnfaktura.setBounds(58, 70, 115, 25);
-		add(btnfaktura);
-
-		btnticket = new JButton("Ticket");
-		btnticket.setBounds(58, 30, 89, 23);
-		add(btnticket);
 		
-		btnpedidos = new JButton("Pedidos");
-		btnpedidos.setBounds(58, 140, 150, 25);
-		add(btnpedidos);
-
-		initializeEvents();
-	}
-
-	private void initializeEvents() {
-		this.btnfaktura.addActionListener(listenerBotonFaktura(this.controladorPanelMenu)); 
-		this.btnpedidos.addActionListener(listenerBotonPedidos(this.controladorPanelMenu));
-		this.btnticket.addActionListener(listenerBotonTicket(this.controladorPanelMenu));
-	}
-
-	private ActionListener listenerBotonFaktura(ControladorPanelMenu controladorPanelMenu) {
-		return new ActionListener() {
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.LIGHT_GRAY);
+		panel.setBounds(0, 0, 450, 300);
+		add(panel);
+		panel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("FAKTURA");
+		btnNewButton.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		btnNewButton.setForeground(Color.BLACK);
+		btnNewButton.setBackground(Color.BLACK);
+		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				controladorPanelMenu.accionadoBottonMostrarPanelFaktura();
 			}
-		};
-	}
-	private ActionListener listenerBotonPedidos(ControladorPanelMenu controladorPanelMenu) {
-		return new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				controladorPanelMenu.accionadoBottonMostrarPanelPedidos();
-			}
-		};
-	}
-	private ActionListener listenerBotonTicket(ControladorPanelMenu controladorPanelMenu) {
-		return new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				controladorPanelMenu.accionadoBottonMostrarPanelTicket();
-			}
-		};
+		});
+		btnNewButton.setBounds(176, 113, 89, 23);
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("TICKET");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		btnNewButton_1.setBackground(Color.BLACK);
+		btnNewButton_1.setBounds(176, 147, 89, 23);
+		panel.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("ESKAERA");
+		btnNewButton_2.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		btnNewButton_2.setBackground(Color.BLACK);
+		btnNewButton_2.setBounds(176, 181, 89, 23);
+		panel.add(btnNewButton_2);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(166, 70, -54, 22);
+		panel.add(textArea);
+		
+		JTextArea txtrAukerak = new JTextArea();
+		txtrAukerak.setFont(new Font("Monospaced", Font.ITALIC, 16));
+		txtrAukerak.setBackground(Color.LIGHT_GRAY);
+		txtrAukerak.setText("AUKERAK:");
+		txtrAukerak.setBounds(45, 45, 89, 23);
+		panel.add(txtrAukerak);
+		
+		JButton btnNewButton_3 = new JButton("SARRATU ");
+		btnNewButton_3.setBackground(Color.BLACK);
+		btnNewButton_3.setForeground(Color.BLACK);
+		btnNewButton_3.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		btnNewButton_3.setBounds(351, 266, 89, 23);
+		panel.add(btnNewButton_3);
+
 	}
 }
