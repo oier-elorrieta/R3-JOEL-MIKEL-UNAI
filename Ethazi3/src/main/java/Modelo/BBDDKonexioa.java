0b8ee1b72a3a7e34aaee6ef9e37031772eb40b7d
Joel <joel.fanode@elorrieta-errekamari.com>
2021-03-07 19:24:25 +0100

    Testak eginda

--- Ethazi3/src/main/java/Modelo/BBDDKonexioa.java
+++ Ethazi3/src/main/java/Modelo/BBDDKonexioa.java
@@ -11,9 +11,6 @@ public class BBDDKonexioa {
 	private static final String URL = "jdbc:mysql://localhost:33060/ethazi3";
 	private static final String CLAVE = "elorrieta";
 
-	//private static final String URL = "jdbc:mysql://localhost:3306/ethazi3";
-	//private static final String CLAVE = "12345678";
-
 	public static Connection getConexion() {
 
 		Connection konekzioa = null;
