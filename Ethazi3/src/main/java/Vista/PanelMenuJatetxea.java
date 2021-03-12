a02624d5a8f771ee2732f4378442a0fe898234bc
unaihernando <unai.hernandosa@elorrieta-errekamari.com>
2021-03-12 14:16:44 +0100

    Jmenuak

--- Ethazi3/src/main/java/Vista/PanelMenuJatetxea.java
+++ Ethazi3/src/main/java/Vista/PanelMenuJatetxea.java
@@ -6,26 +6,32 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;  
+import javax.swing.JButton;
+import javax.swing.JMenu;
 import javax.swing.JPanel;
- 
-import Controlador.ControladorPanelMenuJatetxea; 
+
+import Controlador.ControladorPanelMenuJatetxea;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem; 
 
 @SuppressWarnings("serial")
 public class PanelMenuJatetxea extends JPanel {
 
 	private ControladorPanelMenuJatetxea controladorPanelJatetxea;
-	
+
 	private JButton btnFaktura;
 	private JButton btnTicket;
-	private JButton btnEskaera;
-	private JButton btnSarratu;
-	private JButton btnKomanda;
-	private JButton btnHasiera;
+	private JButton btnEskaera; 
+	private JButton btnKomanda; 
 	private JButton btnHornikuntza;
+	private JMenuBar menuBar;
+	private JMenu menuOperatibitatea;
+	private JMenu menuTop5;
+	private JMenuItem menuItemSarratu;
+	private JMenuItem menuItemDeslogeatu;
 
 	// *****************************************************************************************************************************************************************************************************
-	
+
 	public PanelMenuJatetxea(ControladorPanelMenuJatetxea controladorPanelJatetxea) {
 
 		this.controladorPanelJatetxea = controladorPanelJatetxea;
@@ -35,58 +41,67 @@ public class PanelMenuJatetxea extends JPanel {
 
 		btnTicket = new JButton("TICKET");
 		btnTicket.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		btnTicket.setBounds(10, 11, 185, 113); 
+		btnTicket.setBounds(10, 39, 185, 113); 
 		add(btnTicket);
 
 		btnFaktura = new JButton("FAKTURA");
 		btnFaktura.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		btnFaktura.setBounds(255, 11, 185, 113);
+		btnFaktura.setBounds(255, 39, 185, 113);
 		add(btnFaktura);
 
 		btnEskaera = new JButton("ESKAERA");
 		btnEskaera.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		btnEskaera.setBounds(10, 135, 185, 113);
+		btnEskaera.setBounds(10, 176, 185, 113);
 		add(btnEskaera);
 
-
-		btnSarratu = new JButton("SARRATU ");
-		btnSarratu.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		btnSarratu.setBounds(350, 259, 90, 30);
-		add(btnSarratu);
-		
 		btnKomanda = new JButton("KOMANDA");
 		btnKomanda.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		btnKomanda.setBounds(255, 135, 185, 113);
+		btnKomanda.setBounds(255, 176, 185, 113);
 		add(btnKomanda);
-		
-		btnHasiera = new JButton("DESLOGEATU");
-		btnHasiera.setFont(new Font("Tahoma", Font.ITALIC, 8));
-		btnHasiera.setBounds(10, 259, 90, 30);
-		add(btnHasiera);
-		
+
 		btnHornikuntza = new JButton("+");
 		btnHornikuntza.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		btnHornikuntza.setBounds(204, 11, 41, 237);
+		btnHornikuntza.setBounds(205, 52, 41, 237);
 		add(btnHornikuntza);
+
+		menuBar = new JMenuBar();
+		menuBar.setBounds(24, 11, 400, 22);
+		add(menuBar);
+
+		menuOperatibitatea = new JMenu("Operatibitatea");
+		menuBar.add(menuOperatibitatea);
+		
+		JMenuItem proba = new JMenuItem("nio");
+		menuOperatibitatea.add(proba);
+		
+		menuTop5 = new JMenu("Top5 Salmentak");
+		menuBar.add(menuTop5);
+
+		menuItemDeslogeatu	= new JMenuItem("DESLOGEATU");
+		menuBar.add(menuItemDeslogeatu); 
+
+		menuItemSarratu = new JMenuItem("X"); 
+		menuBar.add(menuItemSarratu);
+		menuItemSarratu.setForeground(Color.RED);
 
 		initializeEvents();
 	}
-	
+
 	// *****************************************************************************************************************************************************************************************************
-	
+
 	private void initializeEvents() {
 		this.btnFaktura.addActionListener(listenerFakturaBotoia(this.controladorPanelJatetxea));
 		this.btnTicket.addActionListener(listenerTicketBotoia(this.controladorPanelJatetxea));
-		this.btnEskaera.addActionListener(listenerEskaeraBotoia(this.controladorPanelJatetxea));
-		this.btnSarratu.addActionListener(listenerSarratuBotoia(this.controladorPanelJatetxea));
-		this.btnHasiera.addActionListener(listenerHasieraBotoia(this.controladorPanelJatetxea));
+		this.btnEskaera.addActionListener(listenerEskaeraBotoia(this.controladorPanelJatetxea)); 
 		this.btnHornikuntza.addActionListener(listenerHornikuntzaBotoia(this.controladorPanelJatetxea));
 		this.btnKomanda.addActionListener(listenerKomandaBotoia(this.controladorPanelJatetxea));
+		this.menuItemSarratu.addActionListener(listenerSarratuBotoia(this.controladorPanelJatetxea));
+		this.menuItemDeslogeatu.addActionListener(listenerHasieraBotoia(this.controladorPanelJatetxea));
 
 	}
-	
+
 	// *****************************************************************************************************************************************************************************************************
-	
+
 	private ActionListener listenerFakturaBotoia(ControladorPanelMenuJatetxea controladorPanelJatetxea) {
 		return new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -94,9 +109,9 @@ public class PanelMenuJatetxea extends JPanel {
 			}
 		};
 	} 
-	
+
 	// *****************************************************************************************************************************************************************************************************
-	
+
 	private ActionListener listenerTicketBotoia(ControladorPanelMenuJatetxea controladorPanelJatetxea) {
 		return new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -104,9 +119,9 @@ public class PanelMenuJatetxea extends JPanel {
 			}
 		};
 	} 
-	
+
 	// *****************************************************************************************************************************************************************************************************
-	
+
 	private ActionListener listenerEskaeraBotoia(ControladorPanelMenuJatetxea controladorPanelJatetxea) {
 		return new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -114,9 +129,9 @@ public class PanelMenuJatetxea extends JPanel {
 			}
 		};
 	} 
-	
+
 	// *****************************************************************************************************************************************************************************************************
-	
+
 	private ActionListener listenerSarratuBotoia(ControladorPanelMenuJatetxea controladorPanelJatetxea) {
 		return new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -124,19 +139,19 @@ public class PanelMenuJatetxea extends JPanel {
 			}
 		};
 	}
-	
+
 	// *****************************************************************************************************************************************************************************************************
-	
-		private ActionListener listenerHornikuntzaBotoia(ControladorPanelMenuJatetxea controladorPanelJatetxea) {
-			return new ActionListener() {
-				public void actionPerformed(ActionEvent arg0) {
-					controladorPanelJatetxea.sakatuHornikuntzaBotoia();
-				}
-			};
-		} 
-	
+
+	private ActionListener listenerHornikuntzaBotoia(ControladorPanelMenuJatetxea controladorPanelJatetxea) {
+		return new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				controladorPanelJatetxea.sakatuHornikuntzaBotoia();
+			}
+		};
+	} 
+
 	// *****************************************************************************************************************************************************************************************************
-	
+
 	private ActionListener listenerHasieraBotoia(ControladorPanelMenuJatetxea controladorPanelJatetxea) {
 		return new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -144,9 +159,9 @@ public class PanelMenuJatetxea extends JPanel {
 			}
 		};
 	}
-	
+
 	// *****************************************************************************************************************************************************************************************************
-	
+
 	private ActionListener listenerKomandaBotoia(ControladorPanelMenuJatetxea controladorPanelJatetxea) {
 		return new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
